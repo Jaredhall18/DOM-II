baa4c53c3b5222b7cbf7e3f8963319c0d7ce48bf
jaredhall18 <jaredhall18@gmail.com>
2021-08-31 12:34:15 -0600

    added MouseOver and MouseOut Events to Nav Bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+//MouseOver Event
+const navHover = document.querySelector('.nav');
+
+navHover.addEventListener('mouseover', event => {
+    event.target.style.color = "#20b2aa"
+});
+
+//MouseOut Event
+navHover.addEventListener('mouseout', event => {
+    event.target.style.color = "black"
+});
