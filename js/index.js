a2365456f97c25f3f78ac091847276709ed1473e
jaredhall18 <jaredhall18@gmail.com>
2021-08-31 13:36:31 -0600

    created strobe effect on background when scrolling

--- js/index.js
+++ js/index.js
@@ -10,3 +10,16 @@ navHover.addEventListener('mouseover', event => {
 navHover.addEventListener('mouseout', event => {
     event.target.style.color = "black"
 });
+
+//Toggle Colors of Welcome to Fun Bus when Page Loads
+const strobeHeader = document.querySelector('body');
+
+const strobeBackground = function() {
+   (strobeHeader.style.backgroundColor == "blue") ? strobeHeader.style.backgroundColor = "yellow" : strobeHeader.style.backgroundColor = "blue";
+}
+
+strobeHeader.addEventListener('wheel', strobeBackground);
+// const strobeEffect = setInterval(strobeBackground, 500);
+const strobeEffect = setInterval;
+
+
